9eca65b4c515193f98f41cb0f808cbcbe5417db4
Paul Dowman <paul@pauldowman.com>
2007-10-08 20:21:20 +0000

    route for homepage to fast controller

--- test/test_app/config/routes.rb
+++ test/test_app/config/routes.rb
@@ -12,6 +12,10 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
   # map.connect '', :controller => "welcome"
+  
+  map.connect '/',
+    :controller => 'fast',
+    :action => 'index'
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
