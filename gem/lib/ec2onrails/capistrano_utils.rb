4e366682f1eae06085b33e0189a750ff9a3f9a77
Paul Dowman <paul@pauldowman.com>
2007-10-11 03:04:12 +0000

    added methods to list hostnames for role

--- gem/lib/ec2onrails/capistrano_utils.rb
+++ gem/lib/ec2onrails/capistrano_utils.rb
@@ -14,5 +14,14 @@ module Ec2onrails
         role newrole_sym, srv_def.host, options
       end
     end
+    
+    # return hostnames for the role named role_sym. It must have the options given or no hostnames will be returned
+    def hostnames_for_role(role_sym, options = {})
+      role = roles[role_sym]
+      unless role
+        return []
+      end
+      role.reject{|s| s.options != options}.collect{|s| s.host}.join(',')
+    end
   end
 end
