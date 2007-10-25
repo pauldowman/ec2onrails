e93d5dc499c7adc2bf327272a06fbae4e6a906a5
Paul Dowman <paul@pauldowman.com>
2007-10-25 00:03:36 +0000

    exit if no app installed

--- server/files/usr/local/ec2onrails/bin/backup_app_db.rb
+++ server/files/usr/local/ec2onrails/bin/backup_app_db.rb
@@ -18,8 +18,9 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require File.join(File.dirname(__FILE__), 's3_lib')
+exit unless File.exists?("/mnt/app/current")
 
+require File.join(File.dirname(__FILE__), 's3_lib')
 
 load_db_config
 load_s3_config
