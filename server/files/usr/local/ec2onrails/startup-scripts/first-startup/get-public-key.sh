dfefe66b28ecba6b891d7abe4922ed7b9193537b
Paul Dowman <paul@pauldowman.com>
2007-10-25 01:49:34 +0000

    undo previous change

--- server/files/usr/local/ec2onrails/startup-scripts/first-startup/get-public-key.sh
+++ server/files/usr/local/ec2onrails/startup-scripts/first-startup/get-public-key.sh
@@ -26,7 +26,7 @@
 
 # Get root's authorized_keys file
 mkdir -p -m 700 /root/.ssh
-/usr/bin/curl http://169.254.169.254/2007-08-29/meta-data/public-keys/0/openssh-key > /root/.ssh/authorized_keys
+/usr/bin/curl http://169.254.169.254/2007-01-19/meta-data/public-keys/0/openssh-key > /root/.ssh/authorized_keys
 
 # In case the http get failed.
 if [ ! -s /root/.ssh/authorized_keys ] ; then
