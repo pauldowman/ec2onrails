c5ee5b2307d28f57093133b0ba68d94ab62ce4b5
Paul Dowman <paul@pauldowman.com>
2007-10-28 16:29:21 +0000

    each instance should have it's own ssh keys

---
+++ server/files/usr/local/ec2onrails/startup-scripts/first-startup/generate-ssh-keys.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#    This file is part of EC2 on Rails.
+#    http://rubyforge.org/projects/ec2onrails/
+#
+#    Copyright 2007 Paul Dowman, http://pauldowman.com/
+#
+#    EC2 on Rails is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    EC2 on Rails is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Each instance should have a new set of ssh keys
+
+rm /etc/ssh/ssh_host_dsa_key
+ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N ''
+
+rm /etc/ssh/ssh_host_rsa_key
+ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
