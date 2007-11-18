e32bf7b231c035f0e6c17ee590d96549bc070887
Paul Dowman <paul@pauldowman.com>
2007-11-18 18:36:00 +0000

    bumped version #

--- gem/lib/ec2onrails/version.rb
+++ gem/lib/ec2onrails/version.rb
@@ -20,7 +20,7 @@ module Ec2onrails #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 9
-    TINY  = 5
+    TINY  = 6
     STRING = [MAJOR, MINOR, TINY].join('.')
     
     AMI_ID_32_BIT = 'ami-86c025ef'
