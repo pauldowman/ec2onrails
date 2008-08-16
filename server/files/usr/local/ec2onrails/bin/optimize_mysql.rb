58baafb06fb416c302299676caeefd3c47a7c505
Adam Greene <adam@6bar8.com>
2008-08-16 11:21:40 -0700

    I'm removing a few of the changes from sebastian, as all_roles should *never* be nil because inject is setting it to [], so something else is going on; but I cannot recreate it on my environment

--- server/files/usr/local/ec2onrails/bin/optimize_mysql.rb
+++ server/files/usr/local/ec2onrails/bin/optimize_mysql.rb
@@ -20,8 +20,8 @@ DEFAULT_CONFIG_LOC = "/etc/mysql/my.cnf"
 
 exit unless in_role?(:db_primary)
 
-local_roles = roles.inject([]){|all_roles, role| all_roles << role.first if !all_roles.nil? && role.last.include?("127.0.0.1")}
-only_db_role = local_roles.size < 2 unless local_roles.nil?
+local_roles = roles.inject([]){|all_roles, role| all_roles << role.first if role.last.include?("127.0.0.1")}
+only_db_role = local_roles.size < 2
 
 
 #lets make a copy of the original...but do not overwrite if it already exists!
