c19979b9fce7bce0d517b9bc903e59da4543ff1f
Adam Greene <adam@6bar8.com>
2008-08-30 08:31:10 -0700

    the inject wasn't working correctly if not all roles included '127.0.0.1', but this new method does

--- server/files/usr/local/ec2onrails/bin/optimize_mysql.rb
+++ server/files/usr/local/ec2onrails/bin/optimize_mysql.rb
@@ -20,7 +20,7 @@ DEFAULT_CONFIG_LOC = "/etc/mysql/my.cnf"
 
 exit unless in_role?(:db_primary)
 
-local_roles = roles.inject([]){|all_roles, role| all_roles << role.first if role.last.include?("127.0.0.1")}
+local_roles = roles.collect{|r| r.first if r.last.include?('127.0.0.1')}.compact
 only_db_role = local_roles.size < 2
 
 
