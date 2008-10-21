e24501776299681f62ce45462d80f127d1f9e5bd
Adam Greene <adam@6bar8.com>
2008-10-21 10:31:00 -0700

    I think this is the right setup for god.  There are a few things that still don't make sense to me (why can't I setup a notify on a lifecycle...if I do but not on the start one, I don't receive any msg) or how to get the event based version working... but this setup seems to work correctly and doesn't flood you with msgs every 5 seconds if a service goes down...

--- server/files/usr/local/ec2onrails/lib/god_helper.rb
+++ server/files/usr/local/ec2onrails/lib/god_helper.rb
@@ -8,13 +8,22 @@ module  GodHelper
 
 
     w.behavior(:clean_pid_file)
+
     w.start_if do |start|
       start.condition(:process_running) do |c|
+        c.notify = {:contacts => ['default'], :category => 'process not started...starting'}
         c.interval = 5.seconds
         c.running = false
-        c.notify = {:contacts => ['default'], :category => 'process exited...restarting'}
       end
     end
+
+    # w.start_if do |start|
+    #   start.condition(:process_running) do |c|
+    #     c.interval = 5.seconds
+    #     c.running = false
+    #     c.notify = {:contacts => ['default'], :category => 'process exited...restarting'}
+    #   end
+    # end
 
     # determine when process has finished starting
     # w.transition([:start, :restart], :up) do |on|
@@ -40,10 +49,10 @@ module  GodHelper
 
   def restart_if_resource_hog(w, options={})
     options = {:memory_usage => 175.megabytes, :cpu_usage => 50.percent}.merge(options)
-
     w.restart_if do |restart|
       if options[:memory_usage]
         restart.condition(:memory_usage) do |c|
+          c.notify = {:contacts => ['default'], :category => "process over #{options[:memory_usage]/1.megabyte}MB.  restarting"}
           c.above = options[:memory_usage]
           c.times = [3,5]
         end
@@ -51,6 +60,7 @@ module  GodHelper
 
       if options[:cpu_usage]
         restart.condition(:cpu_usage) do |c|
+          c.notify = {:contacts => ['default'], :category => "process over #{options[:cpu_usage]*100}%.  restarting"}
           c.above = options[:cpu_usage]
           c.times = 5
         end
@@ -74,17 +84,30 @@ module  GodHelper
   end
 
   def monitor_lifecycle(w)
-    w.transition(:up, :unmonitored) do |on| 
-      on.condition(:flapping) do |c| 
+    # w.transition(:up, :unmonitored) do |on|
+    w.lifecycle do |on|
+      on.condition(:flapping) do |c|
         c.notify = {:contacts => ['default'], :category => 'process flapping...restarting'}
-        c.to_state = [:start, :restart] 
-        c.times = 5 
-        c.within = 60.seconds 
+        c.to_state = [:start, :restart]
+        c.times = 5
+        c.within = 5.minutes
+        c.transition = :unmonitored
         c.retry_in = 10.minutes
         c.retry_times = 5
         c.retry_within = 2.hours
-      end 
+      end
     end
+    # w.lifecycle do |on|
+    #   on.condition(:flapping) do |c| 
+    #     c.notify = {:contacts => ['default'], :category => 'process flapping...restarting'}
+    #     c.to_state = [:start, :restart] 
+    #     c.times = 5 
+    #     c.within = 60.seconds 
+    #     c.retry_in = 10.minutes
+    #     c.retry_times = 5
+    #     c.retry_within = 2.hours
+    #   end 
+    # end
     
     # w.lifecycle do |on|
     #   on.condition(:flapping) do |c|
