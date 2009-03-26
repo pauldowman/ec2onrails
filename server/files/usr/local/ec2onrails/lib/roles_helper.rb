69422e389a72be177efef437fe31e2b1341301dd
Paul Dowman <paul@pauldowman.com>
2009-03-26 10:57:45 -0400

    require 'resolv' (needed by Resolv.getname) and sort require statements.

--- server/files/usr/local/ec2onrails/lib/roles_helper.rb
+++ server/files/usr/local/ec2onrails/lib/roles_helper.rb
@@ -16,11 +16,12 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'erb'
 require 'net/http'
 require 'pp'
+require 'resolv'
 require 'socket'
 require 'yaml'
-require 'erb'
 
 module Ec2onrails
   module RolesHelper
