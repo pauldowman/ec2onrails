d259d8e8fecd41c32f62b57824b10b34787981eb
Paul Dowman <paul@pauldowman.com>
2009-06-25 10:01:09 -0400

    Don't ignore errors when running mysql commands in db:start task. That had been done under the assumption that it was most likely failing because it had been already run but there are many reasons for it possibly failing, the most common seems to be mysql not started yet because it took longer than expected to start up.
    The commands all look like they can be run more than once without ill effect anyway, but I haven't tested that.
    
    But more importantly, if they fail we should know about it and stop the deploy. And if they're failing from being run twice then we should fix the cause of that (check if db exists and exit, or ensure drop is run before create, for example).

--- lib/ec2onrails/recipes/db.rb
+++ lib/ec2onrails/recipes/db.rb
@@ -54,32 +54,17 @@ Capistrano::Configuration.instance.load do
         start
         sleep(15) # make sure the db has some time to start up!
         
-        #NOTE: if these commands fail, it is most likely because the command has already been run....
-
-        cmds = []
-        #sometimes there is a test database that comes with the default installation of MySQL...get rid of it!
-        
-        cmds << %{mysql -u root -e "drop database if exists test; flush privileges;"}
+        run %{mysql -u root -e "drop database if exists test; flush privileges;"}
         # removing anonymous mysql accounts
-        cmds << %{mysql -u root -D mysql -e "delete from db where User = ''; flush privileges;"}
-        cmds << %{mysql -u root -D mysql -e "delete from user where User = ''; flush privileges;"}
+        run %{mysql -u root -D mysql -e "delete from db where User = ''; flush privileges;"}
+        run %{mysql -u root -D mysql -e "delete from user where User = ''; flush privileges;"}
 
         # qoting of database names allows special characters eg (the-database-name)
         # the quotes need to be double escaped. Once for capistrano and once for the host shell
-        cmds << %{mysql -u root -e "create database if not exists \\`#{cfg[:db_name]}\\`;"}
-        cmds << %{mysql -u root -e "grant all on \\`#{cfg[:db_name]}\\`.* to '#{cfg[:db_user]}'@'%' identified by '#{cfg[:db_password]}';"}
-        cmds << %{mysql -u root -e "grant reload on *.* to '#{cfg[:db_user]}'@'%' identified by '#{cfg[:db_password]}';"}
-        cmds << %{mysql -u root -e "grant super on *.* to '#{cfg[:db_user]}'@'%' identified by '#{cfg[:db_password]}';"}
-        
-        cmds.each do |cmd|
-          begin
-            run cmd
-          rescue Exception => e
-              # TODO this is a bit confusing, the command can fail for other reasons, let's improve the message
-              puts "  CONTINUING: this command was previously run, so continuing"
-          end
-        end
-        
+        run %{mysql -u root -e "create database if not exists \\`#{cfg[:db_name]}\\`;"}
+        run %{mysql -u root -e "grant all on \\`#{cfg[:db_name]}\\`.* to '#{cfg[:db_user]}'@'%' identified by '#{cfg[:db_password]}';"}
+        run %{mysql -u root -e "grant reload on *.* to '#{cfg[:db_user]}'@'%' identified by '#{cfg[:db_password]}';"}
+        run %{mysql -u root -e "grant super on *.* to '#{cfg[:db_user]}'@'%' identified by '#{cfg[:db_password]}';"}        
       end
       
       desc <<-DESC
