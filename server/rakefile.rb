fe44302147b4437f3ae7b343b2f1420931583e1d
Paul Dowman <paul@pauldowman.com>
2009-09-25 16:20:47 -0400

    Updated to Rails 2.3.4.

--- server/rakefile.rb
+++ server/rakefile.rb
@@ -87,7 +87,7 @@ require "#{File.dirname(__FILE__)}/../lib/ec2onrails/version"
   "optiflag",
   "passenger",
   "rails",
-  "rails -v '~> 2.3.3'",
+  "rails -v '~> 2.3.4'",
   "rails -v '~> 2.2.2'",
   "rails -v '~> 2.1.2'",
   "rails -v '~> 2.0.5'",
